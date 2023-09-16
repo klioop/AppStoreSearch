c2cca137b8bafcffa332d8e21fbbfe6754e80ce3
sam <sam@teuida.net>
2023-09-16 12:43:51 +0900

    SnapKit spm 추가 AppStoreSearchView 파일생성

---
+++ AppStoreSearchiOS/AppStoreSearchiOS/AppStoreSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  AppStoreSearchView.swift
+//  AppStoreSearchiOS
+//
+//  Created by Lee Sam on 2023/09/16.
+//
+
+import UIKit
+import SnapKit
+
+final class AppStoreSearchView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) { nil }
+}
