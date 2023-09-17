077965dc0971c405e922696f2a7786651b0c24bb
sam <sam@teuida.net>
2023-09-17 17:13:49 +0900

    AppStoreSearchEndPoint 를 프로덕션으로 옮기고 접근제어를 수정함

---
+++ AppStoreSearchiOS/AppStoreSearchiOS/Search API/AppStoreSearchEndPoint.swift
@@ -0,0 +1,47 @@
+//
+//  AppStoreSearchEndPoint.swift
+//  AppStoreSearchiOS
+//
+//  Created by Lee Sam on 2023/09/17.
+//
+
+import Foundation
+
+public enum AppStoreSearchEndPoint {
+    case get(_ searchTerm: String)
+    
+    private enum Query: String, CaseIterable {
+        case country
+        case lang
+        case media
+        case limit
+        
+        var value: String {
+            switch self {
+            case .country: return "kr"
+            case .lang: return "ko-kr"
+            case .media: return "software"
+            case .limit: return "20"
+            }
+        }
+    }
+    
+    public func url() -> URL {
+        let baseURL = URL(string: "https://itunes.apple.com")!
+        
+        switch self {
+        case let .get(term):
+            var components = URLComponents()
+            components.scheme = baseURL.scheme
+            components.host = baseURL.host
+            components.path = path
+            let baseQueries = Query.allCases.map {
+                URLQueryItem(name: $0.rawValue, value: $0.value)
+            }
+            components.queryItems = baseQueries + [URLQueryItem(name: "term", value: term)]
+            return components.url!
+        }
+    }
+    
+    private var path: String { "/search" }
+}
---
+++ AppStoreSearchiOS/AppStoreSearchiOSTests/Search API/AppStoreSearchEndPointTests.swift
@@ -0,0 +1,26 @@
+//
+//  AppStoreSearchEndPointTests.swift
+//  AppStoreSearchiOSTests
+//
+//  Created by Lee Sam on 2023/09/17.
+//
+
+import XCTest
+import AppStoreSearchiOS
+
+class AppStoreSearchEndPointTests: XCTestCase {
+    
+    func test_appStoreSearch_endPoint() {
+        let searchTerm = "search"
+        let url = AppStoreSearchEndPoint.get(searchTerm).url()
+        
+        XCTAssertEqual(url.scheme, "https")
+        XCTAssertEqual(url.host, "itunes.apple.com")
+        XCTAssertEqual(url.path, "/search")
+        XCTAssertEqual(url.query?.contains("lang=ko-kr"), true)
+        XCTAssertEqual(url.query?.contains("country=kr"), true)
+        XCTAssertEqual(url.query?.contains("media=software"), true)
+        XCTAssertEqual(url.query?.contains("limit=20"), true)
+        XCTAssertEqual(url.query?.contains("term=\(searchTerm)"), true)
+    }
+}
