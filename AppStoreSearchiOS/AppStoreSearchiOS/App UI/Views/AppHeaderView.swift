fc7fa69b87f0617353837b8c58da20a195d1b018
sam <sam@teuida.net>
2023-09-17 13:51:20 +0900

    AppHeaderView 에 뒤로가기 타이틀 추가

--- AppStoreSearchiOS/AppStoreSearchiOS/App UI/Views/AppHeaderView.swift
+++ AppStoreSearchiOS/AppStoreSearchiOS/App UI/Views/AppHeaderView.swift
@@ -10,11 +10,37 @@ import SnapKit
 
 public final class AppHeaderView: UIView {
     
-    private lazy var button: UIButton = {
-        let button = UIButton(type: .system)
+    private lazy var container: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 5
+        stack.alignment = .center
+        backImageView.snp.makeConstraints {
+            $0.width.equalTo(18)
+            $0.height.equalTo(24)
+        }
+        [backImageView, label].forEach(stack.addArrangedSubview)
+        return stack
+    }()
+    
+    private lazy var backImageView: UIImageView = {
+        let imageView = UIImageView()
         let image = UIImage(systemName: "chevron.left")
-        button.setImage(image, for: .normal)
-        button.tintColor = .systemGray2
+        imageView.image = image
+        imageView.tintColor = .systemGray2
+        return imageView
+    }()
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 16)
+        label.text = "검색"
+        return label
+    }()
+    
+    private lazy var button: UIButton = {
+        let button = UIButton()
         button.addTarget(self, action: #selector(didTap), for: .touchUpInside)
         return button
     }()
@@ -23,12 +49,12 @@ public final class AppHeaderView: UIView {
         super.init(frame: frame)
         
         backgroundColor = .systemGray6
-        addSubview(button)
-        button.snp.makeConstraints {
+        [container, button].forEach(addSubview)
+        container.snp.makeConstraints {
             $0.verticalEdges.equalToSuperview().inset(16)
-            $0.leading.equalToSuperview().offset(16)
-            $0.width.height.equalTo(24)
+            $0.leading.equalToSuperview().inset(16)
         }
+        button.snp.makeConstraints { $0.edges.equalTo(container.snp.edges) }
     }
     
     required init?(coder: NSCoder) { nil }
