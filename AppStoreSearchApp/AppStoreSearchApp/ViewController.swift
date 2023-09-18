7297189a32780ebcd7f907096bf65c3a68c4ac25
sam <sam@teuida.net>
2023-09-18 18:14:56 +0900

    AppStoreSearchApp 프로젝트를 생성

---
+++ AppStoreSearchApp/AppStoreSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreSearchApp
+//
+//  Created by Lee Sam on 2023/09/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
